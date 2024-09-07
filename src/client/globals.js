a10119c72b105269267173f20fcd863e4791b202
Darren <darrensheng1325@gmail.com>
2024-09-07 10:13:24 -0700

    Add globals

---
+++ src/client/globals.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.globals = void 0;
+var globals = {};
+exports.globals = globals;
---
+++ src/client/globals.ts
@@ -0,0 +1,5 @@
+export {
+  globals,
+};
+var globals = {
+};
